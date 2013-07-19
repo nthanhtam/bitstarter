fb868e6c1547b47ee434effeb251c00a16111ac0
Tam Nguyen <nthanhtam@gmail.com>
2013-07-19 10:19:56 +0800

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var filename = 'index.html';
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile(filename, 'utf8', function(err, data) {
+  	if (err) throw err;
+  		var buf = new Buffer(data);
+  		response.send(buf.toString());
+	});
+
+  
 });
 
 var port = process.env.PORT || 8080;
